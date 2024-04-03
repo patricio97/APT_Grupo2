ed564e71cfdd9d6e8618d7f24dee2211addf6dd0
Patricio Lagos <115107187+patricio97@users.noreply.github.com>
2024-04-02 23:11:41 -0300

    corrigiendo error de rutas

--- barbersync/barbersync_App/src/app/app-routing.module.ts
+++ barbersync/barbersync_App/src/app/app-routing.module.ts
@@ -4,24 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login-barbero',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
-  },
-  {
-    path: 'logi',
-    loadChildren: () => import('./login_registro_barbero/logi/logi.module').then( m => m.LogiPageModule)
   },
   {
     path: 'login-barbero',
@@ -38,7 +26,8 @@ const routes: Routes = [
   {
     path: 'registro-cliente',
     loadChildren: () => import('./registro-cliente/registro-cliente.module').then( m => m.RegistroClientePageModule)
-  },  {
+  },
+  {
     path: 'home1',
     loadChildren: () => import('./home1/home1.module').then( m => m.Home1PageModule)
   }
