0448a8ae3b285383ef683daf8c8c62cdde155eaf
Patricio Lagos <115107187+patricio97@users.noreply.github.com>
2024-04-06 23:46:36 -0300

    s

--- barbersync/barbersync_App/server.js
+++ barbersync/barbersync_App/server.js
@@ -1,74 +1,11 @@
 
-var express = require("express");
-var app = express();
-var router = express.Router();
-var bodyparser=require('body-parser');
-var oracledb = require('oracledb');
-//Authoriser tous les requettes cors)
-var cors = require('cors');
-app.use(cors());
 
-app.use(bodyparser.json());
+const express = require('express');
 
-///Pour changer le format de la requete 
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+const app = express();
 
+require('../barbersync_App/routes')(app);
 
-var connAttrs = {
-    "user": "System",
-    "password": "APT_grupo2",
-    "connectString": "(DESCRIPTION =(LOAD_BALANCE = ON)(FAILOVER = ON)(ADDRESS =(PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)(FAILOVER_MODE=(TYPE=SELECT)(METHOD = BASIC))))"
-}
-
-/////Consulter users////// done
-app.get('/users', function (req, res) {
-    "use strict";
-
-    oracledb.getConnection(connAttrs, function (err, connection) {
-        if (err) {
-            // Error connecting to DB
-            res.set('Content-Type', 'application/json');
-            res.status(500).send(JSON.stringify({
-                status: 500,
-                message: "Error connecting to DB",
-                detailed_message: err.message
-            }));
-            return;
-        }
-        connection.execute("SELECT * FROM all_users", {}, {
-            outFormat: oracledb.OBJECT // Return the result as Object
-        }, function (err, result) {
-            if (err) {
-                res.set('Content-Type', 'application/json');
-                res.status(500).send(JSON.stringify({
-                    status: 500,
-                    message: "Error getting the dba_tablespaces",
-                    detailed_message: err.message
-                }));
-            } else {
-                res.header('Access-Control-Allow-Origin','*');
-                res.header('Access-Control-Allow-Headers','Content-Type');
-                res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
-                res.contentType('application/json').status(200);
-                res.send(JSON.stringify(result.rows));
-				
-            }
-            // Release the connection
-            connection.release(
-                function (err) {
-                    if (err) {
-                        console.error(err.message);
-                    } else {
-                        console.log("GET /sendTablespace : Connection released");
-                    }
-                });
-        });
-    });
-});
-
-
-app.listen(3000,function(){
-    console.log("Live at Port 3000");
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
 });
