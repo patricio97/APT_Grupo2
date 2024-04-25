c750fbc5f5c1bdc88721fc8574fb505dc9527ea1
Patricio Lagos <115107187+patricio97@users.noreply.github.com>
2024-04-25 01:15:22 -0400

    integrando bd

---
+++ barbersync/barbersync_db/database.js
@@ -0,0 +1,19 @@
+const oracledb = require('oracledb');
+
+async function testConnection() {
+  try {
+    const connection = await oracledb.getConnection({
+      user: 'system',
+      password: 'APT_grupo2',
+      connectString: 'localhost:1521/xe' // Ajusta esto según tu configuración
+    });
+
+    console.log("Conectado exitosamente a Oracle Database");
+
+    await connection.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+testConnection();
